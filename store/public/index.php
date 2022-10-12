bfec101275069ed4f901f85450a15238dfb6cd6a
Stlgy <110057231+Stlgy@users.noreply.github.com>
2022-10-12 14:56:44 +0100

    Create index.php

---
+++ store/public/index.php
@@ -0,0 +1,26 @@
+<?php
+    /* header( $_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found'); */
+  
+    require_once __DIR__.'/../vendor/autoload.php';
+
+    /* echo '<pre>';
+    var_dump(dirname(__DIR__));
+    echo'</pre>';
+    exit; */
+    use app\controllers\AboutController;
+    use app\controllers\SiteController;
+    use app\core\Application;
+
+    $app = new Application(dirname(__DIR__));
+
+    $app->router->get('/', [SiteController::class, 'home']);
+    $app->router->get('/contact', [SiteController::class, 'contact']);
+    $app->router->post('/contact', [SiteController::class, 'handleContact']);
+
+    $app->router->get('/login', [AuthController::class,'login']);
+    $app->router->post('/login', [AuthController::class,'login']);  
+    $app->router->get('/register', [AuthController::class,'register']);
+    $app->router->post('/register', [AuthController::class,'register']);
+    $app->run();
+
+?>
