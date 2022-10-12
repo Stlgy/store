b0753cba1f89e12856d2df81f597821e58840779
Stlgy <110057231+Stlgy@users.noreply.github.com>
2022-10-12 14:48:27 +0100

    Create AboutController.php

---
+++ store/controllers/AboutController.php
@@ -0,0 +1,18 @@
+<?php
+
+    namespace app\controllers;
+
+    use thecodeholic\phpmvc\Controller;
+
+    /* Class AboutController
+    
+        @package app\controllers
+     */
+    class AboutController extends AboutController
+    {
+        public function index()
+        {
+            return $this->render('about');
+        }
+    }
+?>
